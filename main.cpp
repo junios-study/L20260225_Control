c6005f25aba4e45a92d5d30d7ec518b0ef6d596a
junios-study <junios@naver.com>
2026-02-25 10:24:26 +0900

    계산

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <conio.h>
+
+
+using namespace std;
+
+int main()
+{
+	int First = 0;
+	int Second = 0;
+
+	cin >> First;
+	cin >> Second;
+
+	cout << First + Second << endl;
+	cout << First - Second << endl;
+	cout << First * Second << endl;
+	cout << First / Second << endl;
+	cout << First % Second << endl;
+
+	return 0;
+}
